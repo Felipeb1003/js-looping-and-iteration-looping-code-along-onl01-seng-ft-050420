e4dfe41a688ad7206214e80dcafe7a3b7e48aebc
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-18 19:03:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solutions in this file
 
-const names = ["Ada", "Brendan", "Ali"];
-const birthday = "birthday";
+
 
 function writeCards(names, birthday) {
   for (let i = 0; i < names.legth; i++) {
