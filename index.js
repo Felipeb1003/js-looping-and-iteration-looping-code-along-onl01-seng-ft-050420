af9dcf0f892f376a6969d22950d9248073d86a0e
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-08-18 19:07:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 
 
-function writeCards(names, birthday) {
+function writeCards( names, birthday) {
   let cards = []
-  for (let i = 0; i < names.legth; i++) {
+  for (let i = 0; i < names.length; i++) {
     cards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
   }
  return cards
 }
+
